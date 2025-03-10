d39d9c27ebb2cf3a66e3b63abc617e1e8127534b
김본 <qhsl0615@naver.com>
2025-03-10 17:38:55 +0900

    INIT: backend init

---
+++ mug-backend/src/main/java/com/example/mugbackend/MugBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mugbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugBackendApplication.class, args);
+	}
+
+}
