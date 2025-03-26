1eed7a428bfd2510760f412dd630df2b6e0d44f4
정연희 <spig0126@naver.com>
2025-03-26 15:35:10 +0900

    CHORE: create domains

---
+++ mug-backend/src/main/java/com/example/mugbackend/transaction/domain/Transaction.java
@@ -0,0 +1,40 @@
+package com.example.mugbackend.transaction.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "transactions")
+public class Transaction {
+	@Id
+	private String id;
+
+	@Field(name = "user_id", targetType = FieldType.OBJECT_ID)
+	private String userId;
+
+	private Integer year;
+	private Integer month;
+	private Integer day;
+	private Boolean isCombined;
+	private String title;
+	private Integer cost;
+	private String category;
+	private String memo;
+	private String vendor;
+	private LocalDateTime time;
+	private String paymentMethod;
+	private List<Transaction> group;
+}
