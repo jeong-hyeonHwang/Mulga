d4e4215399fe35fc618fc61f236312768883fa3b
정연희 <spig0126@naver.com>
2025-03-27 13:35:07 +0900

    FEAT: add fireabse authentication filter(150)
    jira-reg: #S12P21A2-150

---
+++ mug-backend/src/main/java/com/example/mugbackend/common/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.mugbackend.common.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.example.mugbackend.user.firebase.FirebaseAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final FirebaseAuthenticationFilter firebaseAuthenticationFilter;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+			.csrf(AbstractHttpConfigurer::disable)
+			.cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정
+			.addFilterBefore(firebaseAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("*"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
+
---
+++ mug-backend/src/main/java/com/example/mugbackend/user/firebase/FirebaseAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package com.example.mugbackend.user.firebase;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.mugbackend.user.domain.User;
+import com.example.mugbackend.user.dto.CustomUserDetails;
+import com.example.mugbackend.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class FirebaseAuthenticationFilter extends OncePerRequestFilter {
+	private final UserService userService;
+	private final FirebaseService firebaseService;
+
+	@Override
+	protected void doFilterInternal(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		FilterChain filterChain
+	) throws ServletException, IOException {
+		String requestUri = request.getRequestURI();
+		String method = request.getMethod();
+		String authorizationHeader = request.getHeader("Authorization");
+
+		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			try {
+				FirebaseService.DecodedToken decodedToken = firebaseService.parseToken(authorizationHeader);
+				User userOpt = userService.findUserById(decodedToken.uid());
+				CustomUserDetails userDetails = CustomUserDetails.of(userOpt);
+				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+
+			} catch (Exception e) {
+				return;
+			}
+		} else {
+			return;
+		}
+		filterChain.doFilter(request, response);
+	}
+}
