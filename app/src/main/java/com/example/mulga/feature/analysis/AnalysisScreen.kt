fdc9e350683ed888d3236cf22f5ac1fecfad3340
Taehan Yoon <taehan.d.yoon@gmail.com>
2025-03-28 17:39:30 +0900

    FEAT: Added Donut Chart(WIP)

--- app/src/main/java/com/example/mulga/feature/analysis/AnalysisScreen.kt
+++ app/src/main/java/com/example/mulga/feature/analysis/AnalysisScreen.kt
@@ -1,9 +1,14 @@
 package com.example.mulga.feature.analysis
 
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mulga.feature.analysis.components.DonutChart
 
+@Preview
 @Composable
 fun AnalysisScreen() {
     Text(text = "분석 화면")
+    DonutChart()
 }
