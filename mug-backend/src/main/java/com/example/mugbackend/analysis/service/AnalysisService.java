40099293cce4776fc7a8c4bd041334ebefdf1279
정연희 <spig0126@naver.com>
2025-04-02 11:05:01 +0900

    FEAT: apply changes to analysis when transaction change

--- mug-backend/src/main/java/com/example/mugbackend/analysis/service/AnalysisService.java
+++ mug-backend/src/main/java/com/example/mugbackend/analysis/service/AnalysisService.java
@@ -1,11 +1,15 @@
 package com.example.mugbackend.analysis.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.mugbackend.analysis.domain.Analysis;
 import com.example.mugbackend.analysis.dto.AnalysisDetailDto;
 import com.example.mugbackend.analysis.exception.AnalysisNotFoundException;
 import com.example.mugbackend.analysis.repository.AnalysisRepository;
+import com.example.mugbackend.transaction.domain.Transaction;
 import com.example.mugbackend.user.dto.CustomUserDetails;
 
 import lombok.RequiredArgsConstructor;
@@ -23,6 +27,94 @@ public class AnalysisService {
 			.orElseThrow(AnalysisNotFoundException::new);
 		return AnalysisDetailDto.of(analysis);
 	}
+
+	@Transactional
+	public AnalysisDetailDto addTransactionToAnalysis(CustomUserDetails userDetails, Transaction transaction) {
+		Analysis analysis = getAnalysisById(userDetails, transaction.getYear(), transaction.getMonth());
+
+		applyChangeToAnalysis(analysis, transaction, true);
+
+		analysisRepository.updateAnalysis(analysis, transaction);
+
+		return AnalysisDetailDto.of(analysis);
+	}
+
+	@Transactional
+	public AnalysisDetailDto removeTransactionFromAnalysis(CustomUserDetails userDetails, Transaction transaction) {
+		Analysis analysis = getAnalysisById(userDetails, transaction.getYear(), transaction.getMonth());
+
+		applyChangeToAnalysis(analysis, transaction, false);
+
+		analysisRepository.updateAnalysis(analysis, transaction);
+
+		return AnalysisDetailDto.of(analysis);
+	}
+
+	@Transactional
+	public AnalysisDetailDto alterTransactionToAnalysis(CustomUserDetails userDetails, Transaction beforeTransaction, Transaction currentTransaction) {
+		if(!beforeTransaction.getYear().equals(currentTransaction.getYear()) || !(beforeTransaction.getMonth().equals(currentTransaction.getMonth()))) {
+			Analysis beforeAnalysis = getAnalysisById(userDetails, beforeTransaction.getYear(), beforeTransaction.getMonth());
+			Analysis currentAnalysis = getAnalysisById(userDetails, currentTransaction.getYear(), currentTransaction.getMonth());
+
+			applyChangeToAnalysis(beforeAnalysis, beforeTransaction, false);
+			applyChangeToAnalysis(currentAnalysis, currentTransaction, true);
+
+			analysisRepository.updateAnalysis(beforeAnalysis, currentAnalysis, beforeTransaction, currentTransaction);
+
+			return AnalysisDetailDto.of(currentAnalysis);
+		}
+		else {
+
+			Analysis analysis = getAnalysisById(userDetails, currentTransaction.getYear(), currentTransaction.getMonth());
+
+			applyChangeToAnalysis(analysis, beforeTransaction, false);
+			applyChangeToAnalysis(analysis, currentTransaction, true);
+
+			analysisRepository.updateAnalysis(analysis, beforeTransaction, currentTransaction);
+
+			return AnalysisDetailDto.of(analysis);
+		}
+	}
+
+	private void applyChangeToAnalysis(Analysis analysis, Transaction transaction, Boolean isAdded) {
+		Integer cost = transaction.getCost();
+		String category = transaction.getCategory();
+		String paymentMethod = transaction.getPaymentMethod();
+		int day = transaction.getDay();
+
+		Analysis.DailyAmount dailyAmount = analysis.getDaily().get(day);
+		Integer dailyIncome = dailyAmount.getIncome();
+		Integer dailyExpense = dailyAmount.getExpense();
+		Integer paymentExpense = Optional.ofNullable(analysis.getPaymentMethod().get(paymentMethod))
+			.orElse(0);
+		Integer categoryExpense = analysis.getCategory().get(category);
+
+		if(cost < 0) {
+			cost = isAdded? Math.abs(cost): cost;
+			dailyExpense = Math.max(0, dailyExpense + cost);
+			paymentExpense = Math.max(0, paymentExpense + cost);
+			categoryExpense = Math.max(0, categoryExpense + cost);
+
+			analysis.setMonthTotal(analysis.getMonthTotal() + cost);
+			dailyAmount.setExpense(dailyExpense);
+			analysis.getPaymentMethod().put(paymentMethod, paymentExpense);
+			analysis.getCategory().put(category, categoryExpense);
+
+			if(paymentExpense == 0) {
+				analysis.getPaymentMethod().remove(paymentMethod);
+			}
+		}
+		else {
+			cost = isAdded? cost: -cost;
+			dailyIncome = Math.max(0, dailyIncome + cost);
+			dailyAmount.setIncome(dailyIncome);
+		}
+
+		dailyAmount.setIsValid(!(dailyIncome == 0 && dailyExpense == 0));
+	}
+
+
+
 	private Analysis getAnalysisById(CustomUserDetails userDetails, Integer year, Integer month) {
 		String id = String.format("%s_%d_%d", userDetails.id(), year, month);
 
--- mug-backend/src/main/java/com/example/mugbackend/transaction/service/TransactionService.java
+++ mug-backend/src/main/java/com/example/mugbackend/transaction/service/TransactionService.java
@@ -5,33 +5,26 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.mugbackend.analysis.domain.Analysis;
 import com.example.mugbackend.analysis.repository.AnalysisRepository;
+import com.example.mugbackend.analysis.service.AnalysisService;
 import com.example.mugbackend.transaction.domain.Transaction;
-import com.example.mugbackend.transaction.exception.TransactionNoHistoryException;
 import com.example.mugbackend.transaction.dto.TransactionCreateDto;
 import com.example.mugbackend.transaction.dto.TransactionDetailDto;
 import com.example.mugbackend.transaction.dto.TransactionUpdateDto;
+import com.example.mugbackend.transaction.exception.TransactionAccessDeniedException;
+import com.example.mugbackend.transaction.exception.TransactionNoHistoryException;
 import com.example.mugbackend.transaction.exception.TransactionNotFoundException;
 import com.example.mugbackend.transaction.repository.TransactionRepository;
 import com.example.mugbackend.user.dto.CustomUserDetails;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-
-import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -40,6 +33,7 @@ public class TransactionService {
     private final AnalysisRepository analysisRepository;
     private final TransactionRepository transactionRepository;
     private final MongoTemplate mongoTemplate;
+    private final AnalysisService analysisService;
 
     public int getMonthTotal(String userId, int year, int month) {
         String id = userId + "_" + year + "_" + month;
@@ -85,24 +79,44 @@ public class TransactionService {
     public TransactionDetailDto createTransaction(CustomUserDetails userDetails, TransactionCreateDto dto) {
         Transaction transaction = dto.toEntity();
         transaction.setUserId(userDetails.id());
+
+        analysisService.addTransactionToAnalysis(userDetails, transaction);
+
         transactionRepository.save(transaction);
+
         return TransactionDetailDto.of(transaction);
     }
 
     @Transactional
     public TransactionDetailDto updateTransaction(CustomUserDetails userDetails, TransactionUpdateDto dto) {
-        Update update = dto.toUpdate();
-        Criteria criteria = Criteria.where("_id").is(dto.id()).and("userId").is(userDetails.id());
+        Transaction transaction = findById(userDetails, dto.id());
 
         Transaction updatedTransaction = transactionRepository.updateTransaction(userDetails, dto.toEntity())
             .orElseThrow(TransactionNotFoundException::new);
 
+        analysisService.alterTransactionToAnalysis(userDetails, transaction, updatedTransaction);
+
         return TransactionDetailDto.of(updatedTransaction);
     }
 
-    public void deleteTransaction(CustomUserDetails userDetails, List<String> transactionIds) {
+    @Transactional
     public void deleteTransactions(CustomUserDetails userDetails, List<String> transactionIds) {
+        for(String transactionId : transactionIds) {
+            Transaction transaction = findById(userDetails, transactionId);
+            analysisService.removeTransactionFromAnalysis(userDetails, transaction);
+        }
+
         transactionRepository.deleteAllByIdIn(transactionIds);
+    }
+
+    private Transaction findById(CustomUserDetails userDetails, String id) {
+        Transaction transaction = transactionRepository.findById(id)
+            .orElseThrow(() -> new TransactionNotFoundException());
+
+        if(!userDetails.id().equals(transaction.getUserId())) {
+            throw new TransactionAccessDeniedException();
+        }
+        return transaction;
     }
 
 }
